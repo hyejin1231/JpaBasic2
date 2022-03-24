14bfa2b7ca92381b94d83f279c818a214741b5b7
hyejin1231 <phj981231@naver.com>
2022-03-24 20:24:02 +0900

    20220324 상속관계 매핑 & @MappedSuperclass

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -19,9 +20,14 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Order order = new Order();
-            order.addOrderItem(new OrderItem()); // 양방향 매핑, 연관관계 편의 메서드
+//            Order order = new Order();
+//            order.addOrderItem(new OrderItem()); // 양방향 매핑, 연관관계 편의 메서드
 
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("kim");
+
+            em.persist(book);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
---
+++ src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    private String createdBy;
+    private LocalDateTime createDate;
+
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedByDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedByDate() {
+        return lastModifiedByDate;
+    }
+
+    public void setLastModifiedByDate(LocalDateTime lastModifiedByDate) {
+        this.lastModifiedByDate = lastModifiedByDate;
+    }
+}
